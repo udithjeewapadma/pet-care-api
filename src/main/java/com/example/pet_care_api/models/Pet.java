6178a611649f7883d460b1b480337c4996196d0d
Jeewapadma <udithjeewapadma117@gmail.com>
2025-05-10 11:49:50 +0530

    create some models

---
+++ src/main/java/com/example/pet_care_api/models/Pet.java
@@ -0,0 +1,21 @@
+package com.example.pet_care_api.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "pets")
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String petName;
+    private String petBreed;
+    private String petGender;
+    private String petAge;
+    private String petWeight;
+    private String birthDate;
+
+}
---
+++ src/test/java/com/example/pet_care_api/PetCareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pet_care_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
