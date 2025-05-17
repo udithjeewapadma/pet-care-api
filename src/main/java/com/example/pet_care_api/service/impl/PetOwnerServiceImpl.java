d8ef97cdf431d31f13cdd876488fd1afa9318607
Jeewapadma <udithjeewapadma117@gmail.com>
2025-05-17 11:17:41 +0530

    implementing petOwnerServiceImpl and implement createPetOwner method

---
+++ src/main/java/com/example/pet_care_api/service/impl/PetOwnerServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.pet_care_api.service.impl;
+
+import com.example.pet_care_api.controllers.dto.request.CreatePetOwnerRequestDTO;
+import com.example.pet_care_api.models.PetOwner;
+import com.example.pet_care_api.repositories.PetOwnerRepository;
+import com.example.pet_care_api.service.PetOwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PetOwnerServiceImpl implements PetOwnerService {
+
+    @Autowired
+    private PetOwnerRepository petOwnerRepository;
+
+    @Override
+    public PetOwner createPetOwner(CreatePetOwnerRequestDTO createPetOwnerRequestDTO) {
+
+        PetOwner petOwner = new PetOwner();
+        petOwner.setUserName(createPetOwnerRequestDTO.getUserName());
+        petOwner.setAddress(createPetOwnerRequestDTO.getAddress());
+        petOwner.setPhoneNumber(createPetOwnerRequestDTO.getPhoneNumber());
+
+        return petOwnerRepository.save(petOwner);
+    }
+}
