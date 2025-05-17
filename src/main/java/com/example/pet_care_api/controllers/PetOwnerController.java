edc049124dcac1a255b967167381508edb418adc
Jeewapadma <udithjeewapadma117@gmail.com>
2025-05-17 11:29:20 +0530

    implementing postmapping on petOwnerController to createPetOwner

---
+++ src/main/java/com/example/pet_care_api/controllers/PetOwnerController.java
@@ -0,0 +1,31 @@
+package com.example.pet_care_api.controllers;
+
+import com.example.pet_care_api.controllers.dto.request.CreatePetOwnerRequestDTO;
+import com.example.pet_care_api.controllers.dto.response.PetOwnerResponseDTO;
+import com.example.pet_care_api.models.PetOwner;
+import com.example.pet_care_api.service.PetOwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/owners")
+public class PetOwnerController {
+
+    @Autowired
+    private PetOwnerService petOwnerService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PetOwnerResponseDTO createPetOwner(@RequestBody CreatePetOwnerRequestDTO createPetOwnerRequestDTO) {
+
+        PetOwner petOwner = petOwnerService.createPetOwner(createPetOwnerRequestDTO);
+
+        PetOwnerResponseDTO petOwnerResponseDTO = new PetOwnerResponseDTO();
+        petOwnerResponseDTO.setId(petOwner.getId());
+        petOwnerResponseDTO.setUserName(petOwner.getUserName());
+        petOwnerResponseDTO.setAddress(petOwner.getAddress());
+        petOwnerResponseDTO.setPhoneNumber(petOwner.getPhoneNumber());
+        return petOwnerResponseDTO;
+    }
+}
--- src/main/java/com/example/pet_care_api/controllers/dto/response/PetOwnerResponseDTO.java
+++ src/main/java/com/example/pet_care_api/controllers/dto/response/PetOwnerResponseDTO.java
@@ -2,9 +2,10 @@ package com.example.pet_care_api.controllers.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 public class PetOwnerResponseDTO {
 
     private Long id;
