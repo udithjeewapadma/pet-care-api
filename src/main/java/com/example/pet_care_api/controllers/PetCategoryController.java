d9f5764d8b8655291d9540f81ec2a414b8f5e37e
Jeewapadma <udithjeewapadma117@gmail.com>
2025-05-20 11:42:44 +0530

    implementing getMapping to getPetCategoryById method

--- src/main/java/com/example/pet_care_api/controllers/PetCategoryController.java
+++ src/main/java/com/example/pet_care_api/controllers/PetCategoryController.java
@@ -5,10 +5,8 @@ import com.example.pet_care_api.controllers.dto.response.PetCategoryResponseDTO;
 import com.example.pet_care_api.models.PetCategory;
 import com.example.pet_care_api.service.PetCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/categories")
@@ -18,11 +16,17 @@ public class PetCategoryController {
     private PetCategoryService petCategoryService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     private PetCategoryResponseDTO createPetCategory(@RequestBody CreatePetCategoryRequestDTO createPetCategoryRequestDTO) {
         PetCategory petCategory = petCategoryService.createPetCategory(createPetCategoryRequestDTO);
         PetCategoryResponseDTO petCategoryResponseDTO = new PetCategoryResponseDTO();
         petCategoryResponseDTO.setId(petCategory.getId());
         petCategoryResponseDTO.setCategoryName(petCategory.getCategoryName());
         return petCategoryResponseDTO;
+    }
+
+    @GetMapping("/{pet-category-id}")
+    private PetCategoryResponseDTO getPetCategoryById(@PathVariable("pet-category-id") Long petCategoryId) {
+        return petCategoryService.findPetCategoryById(petCategoryId);
     }
 }
