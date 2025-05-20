c1e8a33660eefad8c16263662ebda1c7d3ba8512
Jeewapadma <udithjeewapadma117@gmail.com>
2025-05-20 11:35:18 +0530

    implementing findPetCategoryById method in petCategoryServiceImpl

--- src/main/java/com/example/pet_care_api/service/PetCategoryService.java
+++ src/main/java/com/example/pet_care_api/service/PetCategoryService.java
@@ -1,9 +1,12 @@
 package com.example.pet_care_api.service;
 
 import com.example.pet_care_api.controllers.dto.request.CreatePetCategoryRequestDTO;
+import com.example.pet_care_api.controllers.dto.response.PetCategoryResponseDTO;
 import com.example.pet_care_api.models.PetCategory;
 
 public interface PetCategoryService {
 
     PetCategory createPetCategory(CreatePetCategoryRequestDTO createPetCategoryRequestDTO);
+
+    PetCategoryResponseDTO findPetCategoryById(Long id);
 }
--- src/main/java/com/example/pet_care_api/service/impl/PetCategoryServiceImpl.java
+++ src/main/java/com/example/pet_care_api/service/impl/PetCategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.pet_care_api.service.impl;
 
 import com.example.pet_care_api.controllers.dto.request.CreatePetCategoryRequestDTO;
+import com.example.pet_care_api.controllers.dto.response.PetCategoryResponseDTO;
+import com.example.pet_care_api.exceptions.PetCategoryNotFoundException;
 import com.example.pet_care_api.models.PetCategory;
 import com.example.pet_care_api.repositories.PetCategoryRepository;
 import com.example.pet_care_api.service.PetCategoryService;
@@ -20,5 +22,16 @@ public class PetCategoryServiceImpl implements PetCategoryService {
         petCategory.setCategoryName(createPetCategoryRequestDTO.getCategoryName());
 
         return petCategoryRepository.save(petCategory);
+    }
+
+    @Override
+    public PetCategoryResponseDTO findPetCategoryById(Long id) {
+
+        PetCategory petCategory = petCategoryRepository.findById(id)
+                .orElseThrow( () -> new PetCategoryNotFoundException("Pet Category Not Found"));
+        PetCategoryResponseDTO petCategoryResponseDTO = new PetCategoryResponseDTO();
+        petCategoryResponseDTO.setId(petCategory.getId());
+        petCategoryResponseDTO.setCategoryName(petCategory.getCategoryName());
+        return petCategoryResponseDTO;
     }
 }
