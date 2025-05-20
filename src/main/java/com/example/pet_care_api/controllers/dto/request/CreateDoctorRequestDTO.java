3bfc4d8fa759c4ac2b98ca4916b7fe98d50e4901
Jeewapadma <udithjeewapadma117@gmail.com>
2025-05-20 14:23:26 +0530

    implementing request and response dtos for doctor

---
+++ src/main/java/com/example/pet_care_api/controllers/dto/request/CreateDoctorRequestDTO.java
@@ -0,0 +1,14 @@
+package com.example.pet_care_api.controllers.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+@Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class CreateDoctorRequestDTO {
+
+    private String doctorName;
+    private String qualifications;
+    private int phoneNumber;
+}
---
+++ src/main/java/com/example/pet_care_api/controllers/dto/response/DoctorResponseDTO.java
@@ -0,0 +1,14 @@
+package com.example.pet_care_api.controllers.dto.response;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class DoctorResponseDTO {
+
+    private Long id;
+    private String doctorName;
+    private String qualifications;
+    private int phoneNumber;
+}
