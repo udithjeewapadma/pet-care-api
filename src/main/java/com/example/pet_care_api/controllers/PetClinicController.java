47fbbdba850f3d9197db1183da7ef510ae3e8442
Jeewapadma <udithjeewapadma117@gmail.com>
2025-05-22 08:36:41 +0530

    implementing createPetClinic method in petClinicController

---
+++ src/main/java/com/example/pet_care_api/controllers/PetClinicController.java
@@ -0,0 +1,30 @@
+package com.example.pet_care_api.controllers;
+
+import com.example.pet_care_api.controllers.dto.request.CreatePetClinicRequestDTO;
+import com.example.pet_care_api.controllers.dto.response.PetClinicResponseDTO;
+import com.example.pet_care_api.models.PetClinic;
+import com.example.pet_care_api.service.PetClinicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/clinics")
+public class PetClinicController {
+
+    @Autowired
+    private PetClinicService petClinicService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    private PetClinicResponseDTO createPetClinic(@RequestBody CreatePetClinicRequestDTO createPetClinicRequestDTO) {
+
+        PetClinic petClinic = petClinicService.createPetClinic(createPetClinicRequestDTO);
+        PetClinicResponseDTO petClinicResponseDTO = new PetClinicResponseDTO();
+        petClinicResponseDTO.setId(petClinic.getId());
+        petClinicResponseDTO.setClinicName(petClinic.getClinicName());
+        petClinicResponseDTO.setAddress(createPetClinicRequestDTO.getAddress());
+        petClinicResponseDTO.setPhoneNumber(petClinic.getPhoneNumber());
+        return petClinicResponseDTO;
+    }
+}
