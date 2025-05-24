d1f32ded664ce2c5e865f7e0df61b5746787ad32
Jeewapadma <udithjeewapadma117@gmail.com>
2025-05-24 10:03:10 +0530

    implementing StockCategoryRepository

---
+++ src/main/java/com/example/pet_care_api/repositories/StockCategoryRepository.java
@@ -0,0 +1,7 @@
+package com.example.pet_care_api.repositories;
+
+import com.example.pet_care_api.models.StockCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockCategoryRepository extends JpaRepository<StockCategory, Long> {
+}
