4bffc5eb77d812a1ae388715f1d50bc884e3430d
Jeewapadma <udithjeewapadma117@gmail.com>
2025-05-24 09:32:26 +0530

    doing some changes

--- src/main/java/com/example/pet_care_api/models/Stock.java
+++ src/main/java/com/example/pet_care_api/models/Stock.java
@@ -3,8 +3,6 @@ package com.example.pet_care_api.models;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @Entity
 @Table(name = "stocks")
@@ -13,17 +11,11 @@ public class Stock {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
+    private String description;
+    private String itemCode;
 
     @Enumerated(EnumType.STRING)
     private VisibilityStatus visibilityStatus;
 
-    @ManyToOne
-    @JoinColumn(name = "stock_id")
-    private Stock stock;
-
-    @OneToMany(mappedBy = "stock",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-    private List<FoodStock> foodStocks;
-
-    @OneToMany(mappedBy = "stock",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
-    private List<MedicineStock> medicineStocks;
 }
