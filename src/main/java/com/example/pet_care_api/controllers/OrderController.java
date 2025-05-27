40adc548b33ac4389c5a4ff452fe3c7c79be29c6
Jeewapadma <udithjeewapadma117@gmail.com>
2025-05-27 13:41:38 +0530

    implementing postMapping to createOrder method in orderController

---
+++ src/main/java/com/example/pet_care_api/controllers/OrderController.java
@@ -0,0 +1,33 @@
+package com.example.pet_care_api.controllers;
+
+import com.example.pet_care_api.controllers.dto.request.CreateOrderRequestDTO;
+import com.example.pet_care_api.controllers.dto.response.OrderResponseDTO;
+import com.example.pet_care_api.models.Order;
+import com.example.pet_care_api.models.OrderStatus;
+import com.example.pet_care_api.service.OrderService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+@AllArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public OrderResponseDTO createOrder(@RequestParam Long dealerId,
+                                        @RequestBody CreateOrderRequestDTO createOrderRequestDTO) {
+       Order order = orderService.createOrder(dealerId, createOrderRequestDTO);
+       OrderResponseDTO orderResponseDTO = new OrderResponseDTO();
+       orderResponseDTO.setId(order.getId());
+       orderResponseDTO.setOrderName(order.getOrderName());
+       orderResponseDTO.setOrderStatus(OrderStatus.PENDING);
+       orderResponseDTO.setQuantity(order.getQuantity());
+       orderResponseDTO.setTotalAmount(order.getTotalAmount());
+       orderResponseDTO.setDealerId(order.getDealer().getId());
+       return orderResponseDTO;
+    }
+}
