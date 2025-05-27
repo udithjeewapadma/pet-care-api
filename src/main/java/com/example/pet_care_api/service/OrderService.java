5d47b2dcdc98e23b45901c53dbf6a646ff9d7002
Jeewapadma <udithjeewapadma117@gmail.com>
2025-05-27 13:29:07 +0530

    throwing exception from order service interface

--- src/main/java/com/example/pet_care_api/service/OrderService.java
+++ src/main/java/com/example/pet_care_api/service/OrderService.java
@@ -1,9 +1,10 @@
 package com.example.pet_care_api.service;
 
 import com.example.pet_care_api.controllers.dto.request.CreateOrderRequestDTO;
+import com.example.pet_care_api.exceptions.DealerNotFoundException;
 import com.example.pet_care_api.models.Order;
 
 public interface OrderService {
 
-    Order createOrder(Long dealerId,CreateOrderRequestDTO createOrderRequestDTO);
+    Order createOrder(Long dealerId,CreateOrderRequestDTO createOrderRequestDTO) throws DealerNotFoundException;
 }
